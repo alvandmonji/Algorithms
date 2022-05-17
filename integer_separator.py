e1c645d77b37f190c79ecbc110c164c0d3324424
Alvand <alvandashhadi5@gmail.com>
2022-05-17 21:21:35 +0430

    fix integer separator to dynamic

--- integer_separator.py
+++ integer_separator.py
@@ -1,11 +1,11 @@
-def integer_separator(number):
+def integer_dynamic_separator(number, n):
     str_num = str(number)
     num_list = []
     while str_num.isdigit():
-        num_list.append(str_num[-3:])
-        str_num = str_num[:-3]
+        num_list.append(str_num[-n:])
+        str_num = str_num[:-n]
     return ",".join(reversed(num_list))
 
 
-integer_separator(13858124533)
+integer_dynamic_separator(13858124533, 3)
 "13,858,124,533"
